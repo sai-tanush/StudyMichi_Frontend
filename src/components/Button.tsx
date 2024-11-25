6c4d9d47ad4f38a23f753e4b373ecd0b78a004da
S SAI TANUSH <saitanush789@gmail.com>
2024-11-25 20:36:14 +0530

    chore: added Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom';
+
+interface ButtonProps {
+  children: React.ReactNode;
+  active: boolean;
+  linkto: string;
+}
+
+const Button: React.FC<ButtonProps> = ({ children, active, linkto }) => {
+  return (
+    <Link to={linkto}>
+      <div
+        className={`text-center text-[13px] px-6 py-3 rounded-md font-bold 
+            ${active ? 'bg-yellow-50 text-black' : 'bg-richblack-800 text-white'}`}
+      >
+        {children}
+      </div>
+    </Link>
+  );
+};
+
+export default Button;
