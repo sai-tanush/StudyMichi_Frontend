8411d25bf648f54e9db015f14b2e2b6a7f2a69f5
S SAI TANUSH <saitanush789@gmail.com>
2024-11-26 11:27:53 +0530

    style: added gradient to highlighted text

--- src/components/HighlightText.tsx
+++ src/components/HighlightText.tsx
@@ -3,7 +3,11 @@ interface HighlightTextProps {
 }
 
 const HighlightText: React.FC<HighlightTextProps> = ({ text }) => {
-  return <span className="font-bold text-blue-100">{text}</span>;
+  return (
+    <span className="font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#1FA2FF] via-[#12D8FA] to-[#12D7FA]">
+      {text}
+    </span>
+  );
 };
 
 export default HighlightText;
