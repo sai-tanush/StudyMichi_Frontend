d2fee42c32c8c8ae595080591af5c545c01b5968
S SAI TANUSH <saitanush789@gmail.com>
2024-11-26 20:59:35 +0530

    chore: created Instructor Section component

---
+++ src/components/pages/Homepage/InstructorSection.tsx
@@ -0,0 +1,34 @@
+import Instructor from '../../../assets/Images/Instructor.png';
+import HighlightText from '../../HighlightText';
+import CTAButton from '../../Button';
+import { FaArrowRight } from 'react-icons/fa';
+
+const InstructorSection: React.FC = () => {
+  return (
+    <div className="flex flex-row justify-between gap-10">
+      <div className="w-[80%]">
+        <img src={Instructor} alt="Instructor" className="object-contain" />
+      </div>
+      <div className="w-[55%] flex flex-col justify-center items-start">
+        <div className="text-4xl font-bold">
+          Become an<br></br>
+          <HighlightText text="Instructor" />
+        </div>
+        <div className="font-medium text-base text-richblack-400 mt-4">
+          Instructors from around the world teach millions of students on
+          StudyNotion. We provide the tools and skills to teach what you love.
+        </div>
+        <div className="mt-14">
+          <CTAButton active={true} linkto={'/signup'}>
+            <div className="flex items-center gap-2">
+              Start Teaching Today
+              <FaArrowRight />
+            </div>
+          </CTAButton>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default InstructorSection;
