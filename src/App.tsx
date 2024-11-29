7774c2e9b5d5917fa84ffdb1079d20302b7f19aa
S SAI TANUSH <saitanush789@gmail.com>
2024-11-29 17:11:31 +0530

    chore: added Navbar component to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import './index.css';
 import Homepage from './components/pages/Homepage/Homepage';
+import Navbar from './components/common/Navbar';
 
 function App() {
   return (
     <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/homepage" element={<Homepage />} />
