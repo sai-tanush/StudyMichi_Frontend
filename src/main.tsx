3f7f6891ea13c47b239dcf1ec3a9cb3051806a2d
S SAI TANUSH <saitanush789@gmail.com>
2024-11-29 18:40:13 +0530

    chore: wrapped Provider component to user redux store thorughout

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,20 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import rootReducer from './utils/reducer/index.ts';
+import { configureStore } from '@reduxjs/toolkit';
+
+const store = configureStore({
+  reducer: rootReducer,
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>,
 );
