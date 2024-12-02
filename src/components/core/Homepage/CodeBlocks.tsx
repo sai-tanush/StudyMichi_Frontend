29ecacb1d11100245e7966695559c372ee3ddd99
S SAI TANUSH <saitanush789@gmail.com>
2024-12-02 17:55:33 +0530

    refactor: updated Button component path

--- src/components/core/Homepage/CodeBlocks.tsx
+++ src/components/core/Homepage/CodeBlocks.tsx
@@ -1,6 +1,6 @@
 import { FaArrowRight } from 'react-icons/fa';
 import { TypeAnimation } from 'react-type-animation';
-import CTAButton from './pages/Homepage/Button';
+import CTAButton from './Button';
 
 interface CTAButtonProps {
   btnText: string;
