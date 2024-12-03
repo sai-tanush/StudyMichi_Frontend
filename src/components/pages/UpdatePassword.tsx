20bcaa152642abcd83e70f91d9363aa5e7c4ae48
S SAI TANUSH <saitanush789@gmail.com>
2024-12-03 18:26:06 +0530

    style: styled updatedPassword page component

--- src/components/pages/UpdatePassword.tsx
+++ src/components/pages/UpdatePassword.tsx
@@ -3,8 +3,10 @@ import { RootState } from '../../utils/store/store';
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { resetPassword } from '../../services/operations/authAPI';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai';
+import Spinner from '../common/Spinner';
+import { FaArrowLeftLong } from 'react-icons/fa6';
 
 const UpdatePassword: React.FC = () => {
   const [formData, setFormData] = useState({
@@ -17,6 +19,7 @@ const UpdatePassword: React.FC = () => {
   const { loading } = useSelector((state: RootState) => state.auth);
   const dispatch = useDispatch();
   const location = useLocation();
+  const navigate = useNavigate();
   const { password, confirmPassword } = formData;
 
   const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -29,20 +32,27 @@ const UpdatePassword: React.FC = () => {
   const handleOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const token = location.pathname.split('/').slice(-1)[0];
-    dispatch(resetPassword(password, confirmPassword, token));
+    dispatch(resetPassword(password, confirmPassword, token, navigate));
   };
 
   return (
-    <div>
+    <div className="w-screen h-screen flex justify-center items-center">
       {loading ? (
-        <div>Loading...</div>
+        <Spinner />
       ) : (
-        <div>
-          <h1>Choose new Password</h1>
-          <p>Almost done. Enter your new password and you're all set.</p>
-          <form onSubmit={handleOnSubmit}>
-            <label>
-              <p>New Password</p>
+        <div className="flex flex-col w-1/4 mt-[-100px]">
+          <h1 className="text-richblack-5 text-3xl font-semibold mb-3">
+            Choose new Password
+          </h1>
+          <p className="text-richblack-200">
+            Almost done. Enter your new password and you're all set.
+          </p>
+          <form onSubmit={handleOnSubmit} className="flex flex-col mt-4">
+            <label className="block">
+              <p className="text-richblack-25 text-sm inline-flex items-center">
+                New Password
+                <span className="text-red-300 ml-1">*</span>
+              </p>
               <input
                 required
                 type={showPassword ? 'text' : 'password'}
@@ -50,6 +60,7 @@ const UpdatePassword: React.FC = () => {
                 value={password}
                 onChange={handleOnChange}
                 placeholder="Enter new password"
+                className="w-full p-3.5 mt-1 rounded-lg bg-richblack-700 text-richblack-5"
               />
               <span onClick={() => setShowPassword((prev) => !prev)}>
                 {showPassword ? (
@@ -59,15 +70,19 @@ const UpdatePassword: React.FC = () => {
                 )}
               </span>
             </label>
-            <label>
-              <p>Confirm Password</p>
+            <label className="block">
+              <p className="text-richblack-25 text-sm inline-flex items-center">
+                Confirm new password
+                <span className="text-red-300 ml-1">*</span>
+              </p>
               <input
                 required
                 type={showPassword ? 'text' : 'password'}
-                name="confirmpassword"
+                name="confirmPassword"
                 value={confirmPassword}
                 onChange={handleOnChange}
                 placeholder="Confirm password"
+                className="w-full p-3.5 mt-1 rounded-lg bg-richblack-700 text-richblack-5"
               />
               <span onClick={() => setShowConfirmPassword((prev) => !prev)}>
                 {showConfirmPassword ? (
@@ -78,10 +93,18 @@ const UpdatePassword: React.FC = () => {
               </span>
             </label>
 
-            <button type="submit">Reset Password</button>
+            <button
+              type="submit"
+              className="w-full text-center text-[16px] px-6 py-3 rounded-md font-semibold mt-5
+                                 bg-yellow-50 text-black hover:scale-95 transition-all duration-200 
+                                 shadow-sm shadow-richblack-300"
+            >
+              Reset Password
+            </button>
           </form>
-          <div>
-            <Link to="/login">
+          <div className="text-richblack-200 text-sm mt-5 ml-2">
+            <Link to="/login" className="flex gap-x-1 items-center">
+              <FaArrowLeftLong />
               <p>Back to Login</p>
             </Link>
           </div>
