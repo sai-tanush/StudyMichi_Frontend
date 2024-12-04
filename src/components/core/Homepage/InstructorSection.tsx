08967e500d587a4ae4783c5aae03b9c0fe4e874a
S SAI TANUSH <saitanush789@gmail.com>
2024-12-04 19:54:13 +0530

    refactor: changed import path in InstructorSection

--- src/components/core/Homepage/InstructorSection.tsx
+++ src/components/core/Homepage/InstructorSection.tsx
@@ -1,7 +1,7 @@
 import { FaArrowRight } from 'react-icons/fa';
 import Instructor from '../../../assets/Images/Instructor.png';
 import HighlightText from './HighlightText';
-import CTAButton from './Button';
+import CTAButton from '../../common/Button';
 
 const InstructorSection: React.FC = () => {
   return (
