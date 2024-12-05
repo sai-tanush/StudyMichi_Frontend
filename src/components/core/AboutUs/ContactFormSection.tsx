687bbd7fd0f8ef274a4ccef4827d546746b1459c
S SAI TANUSH <saitanush789@gmail.com>
2024-12-05 09:57:48 +0530

    style: sanitation in bottom-margin in ContactFormSection

--- src/components/core/AboutUs/ContactFormSection.tsx
+++ src/components/core/AboutUs/ContactFormSection.tsx
@@ -2,7 +2,7 @@ import ContactUsForm from '../../common/ContactUsForm';
 
 const ContactFormSection = () => {
   return (
-    <div className="lg:w-1/2 flex flex-col gap-y-4 text-center mx-auto mb-48">
+    <div className="lg:w-1/2 flex flex-col gap-y-4 text-center mx-auto mb-28">
       <div className="flex flex-col gap-y-2 mb-10">
         <h1 className="text-4xl font-bold text-richblack-5">Get in Touch</h1>
         <p className="text-richblack-400 text-md font-semibold">
