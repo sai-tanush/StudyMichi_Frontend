0510e047fdb65f353cf3a2c9ca023a1b77390975
S SAI TANUSH <saitanush789@gmail.com>
2024-12-05 09:46:09 +0530

    chore: created ContactForm Component to be used when user wants to fill details and contact us

---
+++ src/components/common/ContactUsForm.tsx
@@ -0,0 +1,222 @@
+import { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { apiConnector } from '../../services/apisconnector';
+import { contactusEndpoint } from '../../services/apis';
+import CountryCodes from '../../data/countrycode.json';
+import Spinner from './Spinner';
+
+interface FormData {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phoneNumber: string;
+  message: string;
+  countryCode?: string;
+}
+
+const ContactUsForm: React.FC = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const {
+    register,
+    reset,
+    handleSubmit,
+    formState: { errors, isSubmitSuccessful },
+  } = useForm<FormData>();
+
+  useEffect(() => {
+    if (isSubmitSuccessful) {
+      reset({
+        firstName: '',
+        lastName: '',
+        email: '',
+        phoneNumber: '',
+        message: '',
+      });
+    }
+  }, [reset, isSubmitSuccessful]);
+
+  const submitContactForm = async (data: FormData) => {
+    console.log('About Us Form Data = ', data);
+    try {
+      setLoading(true);
+      const response = await apiConnector({
+        method: 'POST',
+        url: contactusEndpoint.CONTACT_US_API,
+        bodyData: data,
+      });
+      console.log('Logging AboutUs Page response = ', response);
+      setLoading(false);
+    } catch (error) {
+      console.log('Error occurred = ', error.message);
+      setLoading(false);
+    }
+  };
+  return (
+    <div>
+      {loading ? (
+        <div className="w-[20%] flex justify-center items-center mx-auto lg:-mt-[200px]">
+          <Spinner />
+        </div>
+      ) : (
+        <form onSubmit={handleSubmit(submitContactForm)}>
+          {/* name  */}
+          <div className="flex justify-between gap-x-5 mb-5">
+            <div className="flex flex-col gap-y-2 w-[48%] ">
+              <label
+                htmlFor="firstname"
+                className="text-richblack-25 text-sm text-left"
+              >
+                First Name
+              </label>
+              <input
+                type="text"
+                id="firstname"
+                placeholder="Enter First Name"
+                className="bg-richblack-800 p-3 rounded-lg text-richblack-100"
+                {...register('firstName', {
+                  required: 'First name is required',
+                })}
+              />
+              {errors.firstName && (
+                <span className="text-sm text-red-500 mt-1">
+                  P{errors.firstName.message}
+                </span>
+              )}
+            </div>
+            <div className="flex flex-col gap-y-2 w-[48%]">
+              <label
+                htmlFor="lastname"
+                className="text-richblack-25 text-sm text-left"
+              >
+                Last Name
+              </label>
+              <input
+                type="text"
+                id="lastname"
+                placeholder="Enter Last Name"
+                className="bg-richblack-800 p-3 rounded-lg text-richblack-100"
+                {...register('lastName', { required: 'Last name is required' })}
+              />
+              {errors.lastName && (
+                <span className="text-sm text-red-500 mt-1">
+                  P{errors.lastName.message}
+                </span>
+              )}
+            </div>
+          </div>
+
+          {/* email */}
+          <div className="flex flex-col gap-y-2 mb-5">
+            <label
+              htmlFor="email"
+              className="text-richblack-25 text-sm text-left"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              placeholder="Enter email address"
+              className="bg-richblack-800 p-3 rounded-lg text-richblack-100"
+              {...register('email', {
+                required: 'Email is required',
+                pattern: {
+                  value: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/,
+                  message: 'Enter a valid email address',
+                },
+              })}
+            />
+            {errors.email && (
+              <p className="text-sm text-red-500 mt-1">
+                {errors.email.message}
+              </p>
+            )}
+          </div>
+
+          {/* phoneNumber */}
+          <div className="flex flex-col gap-2">
+            <label
+              htmlFor="phonenumber"
+              className="text-richblack-25 text-sm text-left"
+            >
+              Phone Number
+            </label>
+
+            <div className="flex flex-row gap-5">
+              {/* dropdown */}
+              <div>
+                <select
+                  id="dropdown"
+                  className="bg-richblack-800 p-3 rounded-lg text-richblack-100 w-[120px]"
+                  defaultValue="+91-India"
+                  {...register('countryCode', { required: true })}
+                >
+                  {CountryCodes.map((country, index) => {
+                    return (
+                      <option key={index}>
+                        {country.code} - {country.country}
+                      </option>
+                    );
+                  })}
+                </select>
+              </div>
+
+              {/* phoneNumber */}
+              <input
+                type="text"
+                id="phonenumber"
+                placeholder="12345 56789"
+                className="bg-richblack-800 p-3 rounded-lg text-richblack-100"
+                {...register('phoneNumber', {
+                  required: 'Phone Number is required',
+                  minLength: { value: 8, message: 'Invalid Phone Number' },
+                  maxLength: { value: 10, message: 'Invalid Phone Number' },
+                  pattern: {
+                    value: /^[0-9]{10}$/,
+                    message: 'Enter a valid 10-digit phone number',
+                  },
+                })}
+              />
+              {errors.phoneNumber && (
+                <p className="text-sm text-red-500 mt-1">
+                  {errors.phoneNumber.message}
+                </p>
+              )}
+            </div>
+          </div>
+
+          {/* message */}
+          <div className="flex flex-col gap-y-2 mb-5">
+            <label
+              htmlFor="message"
+              className="text-richblack-25 text-sm text-left"
+            >
+              Message
+            </label>
+            <textarea
+              id="message"
+              cols={30}
+              rows={7}
+              placeholder="Enter your message"
+              className="bg-richblack-800 p-3 rounded-lg text-richblack-100"
+              {...register('message', { required: 'Message is required' })}
+            />
+            {errors.message && (
+              <span className="text-sm text-red-500 mt-1">
+                {errors.message.message}
+              </span>
+            )}
+          </div>
+          <button
+            type="submit"
+            className="w-full mt-6 rounded-[8px] bg-yellow-50 py-[8px] px-[12px] font-medium text-richblack-900"
+          >
+            Send Message
+          </button>
+        </form>
+      )}
+    </div>
+  );
+};
+
+export default ContactUsForm;
