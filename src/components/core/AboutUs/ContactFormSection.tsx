3c76af8befc9bd9a780c77324ca9238fdc639300
S SAI TANUSH <saitanush789@gmail.com>
2024-12-05 10:14:59 +0530

    refactor: added props in COntactFormSection to be used as a common component

--- src/components/core/AboutUs/ContactFormSection.tsx
+++ src/components/core/AboutUs/ContactFormSection.tsx
@@ -1,13 +1,19 @@
 import ContactUsForm from '../../common/ContactUsForm';
 
-const ContactFormSection = () => {
+interface ContactFormSectionProps {
+  title: string;
+  paragraph: string;
+}
+
+const ContactFormSection: React.FC<ContactFormSectionProps> = ({
+  title,
+  paragraph,
+}) => {
   return (
-    <div className="lg:w-1/2 flex flex-col gap-y-4 text-center mx-auto mb-28">
+    <div className="lg:w-1/2 flex flex-col gap-y-4 text-center mx-auto mb-28 border border-richblack-400 p-10 rounded-xl">
       <div className="flex flex-col gap-y-2 mb-10">
-        <h1 className="text-4xl font-bold text-richblack-5">Get in Touch</h1>
-        <p className="text-richblack-400 text-md font-semibold">
-          We'd love to here for you, Please fill out this form.
-        </p>
+        <h1 className="text-4xl font-bold text-richblack-5">{title}</h1>
+        <p className="text-richblack-400 text-md font-semibold">{paragraph}</p>
       </div>
       <div>
         <ContactUsForm />
