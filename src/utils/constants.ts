10b71f785107b0d88968546ec21fce7373adb531
S SAI TANUSH <saitanush789@gmail.com>
2024-12-09 19:31:31 +0530

    chore: added type of COURSE_STATUS props

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -14,6 +14,12 @@ export interface ACCOUNT_TYPE_PROPS {
   INSTRUCTOR: 'Instructor';
   ADMIN: 'Admin';
 }
+
+export interface COURSE_STATUS_PROPS {
+  DRAFT: 'Draft';
+  PUBLISHED: 'Published';
+}
+
 export const ACCOUNT_TYPE: ACCOUNT_TYPE_PROPS: ACCOUNT_TYPE_PROPS = {
   STUDENT: 'Student',
   INSTRUCTOR: 'Instructor',
