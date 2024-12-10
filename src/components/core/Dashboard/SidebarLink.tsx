f27b0d08290f4ef8f0d2cd4461e1666d1747363a
S SAI TANUSH <saitanush789@gmail.com>
2024-12-10 16:01:52 +0530

    style: sanitation checks in margins

--- src/components/core/Dashboard/SidebarLink.tsx
+++ src/components/core/Dashboard/SidebarLink.tsx
@@ -29,10 +29,14 @@ const SidebarLink: React.FC<SidebarLinkProps> = ({ link, iconName }) => {
     if (route) return matchPath({ path: route }, location.pathname);
   };
   return (
-    <div>
+    <div
+      className={`mt-3 ${matchRoute({ route: link.path }) ? 'bg-yellow-800' : 'bg-opacity-0'} text-richblack-300`}
+    >
       <NavLink
         to={link.path}
-        className={`relative px-8 py-2 text-sm font-medium ${matchRoute({ route: link.path }) ? 'bg-yellow-800' : 'bg-opacity-0'}`}
+        className={`relative px-8 py-2 text-sm font-medium 
+          ${matchRoute({ route: link.path }) ? 'bg-yellow-800' : 'bg-opacity-0'}
+          flex`}
       >
         <span
           className={`absolute left-0 top-0 h-full w-[0.2rem] bg-yellow-50
