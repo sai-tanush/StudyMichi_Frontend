74b2a2658f44d6ca31f3b90bbde96e7511a6fea9
S SAI TANUSH <saitanush789@gmail.com>
2024-12-10 12:27:45 +0530

    chore: added EnrolledCourses wrapped under PrivateRoute Dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import MyProfile from './components/core/Dashboard/MyProfile';
 import Dashboard from './components/pages/Dashboard';
 import PrivateRoute from './components/core/Auth/PrivateRoute';
 import Error from './components/pages/Error';
+import EnrolledCourses from './components/core/Dashboard/EnrolledCourses';
 
 function App() {
   return (
@@ -86,6 +87,10 @@ function App() {
           }
         >
           <Route path="dashboard/my-profile" element={<MyProfile />} />
+          <Route
+            path="dashboard/enrolled-courses"
+            element={<EnrolledCourses />}
+          />
         </Route>
 
         <Route path="*" element={<Error />} />
