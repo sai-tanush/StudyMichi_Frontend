39b6ab5cec6d5b699016aa3103a008208441f854
S SAI TANUSH <saitanush789@gmail.com>
2024-12-11 19:34:04 +0530

    refactor: removed code for updating profilePicture as a separate function for updating ProfilePicture is already written

--- src/services/operations/settingsAPI.ts
+++ src/services/operations/settingsAPI.ts
@@ -63,12 +63,10 @@ export function updateProfile(token: string, formData) {
       if (!response.data.success) {
         throw new Error(response.data.message);
       }
-      const userImage = response.data.updatedUserDetails.image
-        ? response.data.updatedUserDetails.image
-        : `https://api.dicebear.com/5.x/initials/svg?seed=${response.data.updatedUserDetails.firstName} ${response.data.updatedUserDetails.lastName}`;
-      dispatch(
-        setUser({ ...response.data.updatedUserDetails, image: userImage }),
-      );
+      // const userImage = response.data.updatedUserDetails.image
+      //   ? response.data.updatedUserDetails.image
+      //   : `https://api.dicebear.com/5.x/initials/svg?seed=${response.data.updatedUserDetails.firstName} ${response.data.updatedUserDetails.lastName}`;
+      dispatch(setUser({ ...response.data.updatedUserDetails }));
       toast.success('Profile Updated Successfully');
     } catch (error) {
       console.log('UPDATE_PROFILE_API API ERROR............', error);
