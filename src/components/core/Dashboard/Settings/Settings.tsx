e66f5c1153949218fb8f70cb622b05422778e0b3
S SAI TANUSH <saitanush789@gmail.com>
2024-12-11 09:40:12 +0530

    chore: created Settings component

---
+++ src/components/core/Dashboard/Settings/Settings.tsx
@@ -0,0 +1,14 @@
+import ChangeProfilePicture from './ChangeProfilePicture';
+
+const Settings = () => {
+  return (
+    <div>
+      <h1 className="mb-14 text-3xl font-medium text-richblack-5">
+        Edit Profile
+      </h1>
+      <ChangeProfilePicture />
+    </div>
+  );
+};
+
+export default Settings;
