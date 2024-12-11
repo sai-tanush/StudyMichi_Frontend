71d96634498a56c67a3532b4dbc074020ce27bfb
S SAI TANUSH <saitanush789@gmail.com>
2024-12-11 11:10:42 +0530

    style: sanitation checks in Settings component

--- src/components/core/Dashboard/Settings/Settings.tsx
+++ src/components/core/Dashboard/Settings/Settings.tsx
@@ -6,13 +6,15 @@ import UpdateSettingsPassword from './UpdateSettingsPassword';
 const Settings = () => {
   return (
     <div>
-      <h1 className="mb-14 text-3xl font-medium text-richblack-5">
+      <h1 className="mb-7 text-4xl text-left font-medium text-richblack-5">
         Edit Profile
       </h1>
-      <ChangeProfilePicture />
-      <EditProfile />
-      <UpdateSettingsPassword />
-      <DeleteAccount />
+      <div className="w-2/3 flex flex-col gap-y-5 mx-[7%] px-[7%]">
+        <ChangeProfilePicture />
+        <EditProfile />
+        <UpdateSettingsPassword />
+        <DeleteAccount />
+      </div>
     </div>
   );
 };
