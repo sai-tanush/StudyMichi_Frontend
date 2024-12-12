f2a2205bf4c392c7bf42562660a4ce1ba7e55d70
S SAI TANUSH <saitanush789@gmail.com>
2024-12-12 14:46:03 +0530

    chore: added functionality in AddCourse component

--- src/components/core/Dashboard/AddCourse/AddCourse.tsx
+++ src/components/core/Dashboard/AddCourse/AddCourse.tsx
@@ -1,5 +1,25 @@
+import { codeUploadTips } from '../../../../data/code-upload-tips';
+import AddCourseStepForm from './AddCourseStepForm';
+
 const AddCourse: React.FC = () => {
-  return <div></div>;
+  return (
+    <div className="w-screen min-h-screen text-white flex justify-around items-start">
+      <div className="flex flex-col">
+        <h1>Add Course</h1>
+        <div>
+          <AddCourseStepForm />
+        </div>
+      </div>
+      <div className="mr-36">
+        <p>Code Upload Tips</p>
+        <ul>
+          {codeUploadTips.map((tip) => {
+            return <li key={tip.id}>{tip.content}</li>;
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default AddCourse;
