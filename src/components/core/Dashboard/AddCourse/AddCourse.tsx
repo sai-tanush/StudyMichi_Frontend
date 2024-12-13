0f336045d85b2901bc112b811a0d61847c5f8e11
S SAI TANUSH <saitanush789@gmail.com>
2024-12-13 08:43:24 +0530

    style: added styling to <li> and few sanitation margin checks in codeUploadTips data in AddCourse component

--- src/components/core/Dashboard/AddCourse/AddCourse.tsx
+++ src/components/core/Dashboard/AddCourse/AddCourse.tsx
@@ -3,18 +3,22 @@ import AddCourseStepForm from './AddCourseStepForm';
 
 const AddCourse: React.FC = () => {
   return (
-    <div className="w-screen min-h-screen text-white flex justify-around items-start">
-      <div className="flex flex-col">
+    <div className="w-screen min-h-screen text-white flex gap-x-10 items-start">
+      <div className="flex flex-col w-[50%] mx-5">
         <h1>Add Course</h1>
         <div>
           <AddCourseStepForm />
         </div>
       </div>
-      <div className="mr-36">
-        <p>Code Upload Tips</p>
-        <ul>
+      <div className="w-1/5 p-6 bg-richblack-800 rounded-lg">
+        <p className="text-richblack-5 font-semibold">⚡ Code Upload Tips</p>
+        <ul className="list-disc pl-4">
           {codeUploadTips.map((tip) => {
-            return <li key={tip.id}>{tip.content}</li>;
+            return (
+              <li key={tip.id} className="text-xs my-3 font-medium">
+                {tip.content}
+              </li>
+            );
           })}
         </ul>
       </div>
