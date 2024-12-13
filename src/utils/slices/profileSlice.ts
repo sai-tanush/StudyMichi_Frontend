0050d445c72d332eb5ca024a56a2e87c11180e8a
S SAI TANUSH <saitanush789@gmail.com>
2024-12-13 18:39:46 +0530

    chore: added userTypes to profileSlice.ts

--- src/utils/slices/profileSlice.ts
+++ src/utils/slices/profileSlice.ts
@@ -1,6 +1,37 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-const initialState = {
+export interface AdditionalDetailsProps {
+  _id: string;
+  gender: string | null;
+  dateOfBirth: string | null;
+  about: string | null;
+  __v: number;
+}
+
+export interface UserProps {
+  _id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  accountType: 'Instructor' | 'Student' | 'Admin';
+  active: boolean;
+  approved: boolean;
+  additionalDetails: AdditionalDetailsProps;
+  courses: string[]; // Array of course IDs
+  image: string; // URL to the user's image
+  courseProgress: any[]; // Replace `any`
+  createdAt: string; // ISO date string
+  updatedAt: string; // ISO date string
+  __v: number;
+  token: string; // JWT token
+}
+
+interface initialStateProps {
+  user: UserProps;
+  loading: boolean;
+}
+
+const initialState: initialStateProps = {
   user: localStorage.getItem('user')
     ? JSON.parse(localStorage.getItem('user') as string)
     : null,
