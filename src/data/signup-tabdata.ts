ab2cc752ebd57a347e4602a5ccbb988c848bc524
S SAI TANUSH <saitanush789@gmail.com>
2024-12-17 22:52:18 +0530

    chore: created new data file to keep tab data constants

---
+++ src/data/signup-tabdata.ts
@@ -0,0 +1,15 @@
+import { ACCOUNT_TYPE } from '../utils/constants';
+
+// data to pass to Tab component
+export const TAB_DATA = [
+  {
+    id: 1,
+    tabName: 'Student',
+    type: ACCOUNT_TYPE.STUDENT,
+  },
+  {
+    id: 2,
+    tabName: 'Instructor',
+    type: ACCOUNT_TYPE.INSTRUCTOR,
+  },
+];
