7949f83faade699c8bbe134325007ed0c33a1abb
S SAI TANUSH <saitanush789@gmail.com>
2024-12-17 22:45:25 +0530

    refactor: made changed in earninggrid component with CardContent componnet

--- src/components/core/AboutUs/LearningGrid.tsx
+++ src/components/core/AboutUs/LearningGrid.tsx
@@ -1,48 +1,16 @@
 import learningGridData from '../../../data/learningGridData';
-import HighlightText from '../Homepage/HighlightText';
-import CTAButton from '../../common/Button';
+import { getCardClassName } from '../../../utils/helperFunction';
+import CardContent from './CardContent'; // Adjust the path if needed
 
 const LearningGrid: React.FC = () => {
   return (
     <div className="grid mx-auto grid-col-1 lg:grid-cols-4 mb-48 mt-24">
       {learningGridData.map((card, index) => {
+        const className = getCardClassName(index, card);
+
         return (
-          <div
-            key={index}
-            className={`${index === 0 && 'lg:col-span-2 lg:h-[250px]'}
-            ${
-              card.order % 2 === 1
-                ? 'bg-richblack-700 lg:h-[250px]'
-                : 'bg-richblack-900 lg:h-[250px]'
-            }
-            ${card.order === 3 && 'lg:col-start-2'}
-            `}
-          >
-            {card.order < 0 ? (
-              <div className="lg:w-[80%] flex flex-col pb-5 gap-3 ml-20">
-                <div className="text-4xl text-left font-semibold text-richblack-5">
-                  {card.heading} <br></br>
-                  <HighlightText text={card.highlightText} />
-                </div>
-                <p className="text-richblack-400 text-sm text-left font-semibold">
-                  {card.description}
-                </p>
-                <div className="w-fit lg:mt-8">
-                  <CTAButton active={true} linkto={card.BtnLink}>
-                    {card.BtnText}
-                  </CTAButton>
-                </div>
-              </div>
-            ) : (
-              <div className="flex flex-col items-center gap-5 py-8 lg:w-[80%] mx-auto lg:mt-2">
-                <h1 className="text-richblack-5 font-semibold text-left ">
-                  {card.heading}
-                </h1>
-                <p className="text-richblack-50 text-sm text-left font-semibold">
-                  {card.description}
-                </p>
-              </div>
-            )}
+          <div key={index} className={className}>
+            <CardContent card={card} />
           </div>
         );
       })}
