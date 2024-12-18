da3f305ee547aac129eef68072c1c4b8a9ca02d2
S SAI TANUSH <saitanush789@gmail.com>
2024-12-18 12:11:11 +0530

    chore: created aboutus-dta file to keep constant data

---
+++ src/data/aboutus-data.ts
@@ -0,0 +1,6 @@
+export const STATS_DATA = [
+  { count: '20K+', label: 'Active Students' },
+  { count: '10+', label: 'Members' },
+  { count: '200+', label: 'Courses' },
+  { count: '50+', label: 'Awards' },
+];
