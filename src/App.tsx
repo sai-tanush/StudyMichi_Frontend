4786ddfaa7fff5092e4750daf93985e7b21c9c5f
S SAI TANUSH <saitanush789@gmail.com>
2024-12-18 13:01:11 +0530

    chore: sanitation checks in App component

--- src/App.tsx
+++ src/App.tsx
@@ -10,40 +10,19 @@ import UpdatePassword from './components/pages/UpdatePassword';
 import VerifyEmail from './components/pages/VerifyEmail';
 import AboutUs from './components/pages/AboutUs';
 import ContactUs from './components/pages/ContactUs';
-import MyProfile from './components/core/Dashboard/MyProfile';
-import Dashboard from './components/pages/Dashboard';
-import PrivateRoute from './components/core/Auth/PrivateRoute';
-import Error from './components/pages/Error';
-<<<<<<< HEAD
-import EnrolledCourses from './components/core/Dashboard/EnrolledCourses';
-import PurchaseHistory from './components/core/Dashboard/PurchaseHistory';
-import Settings from './components/core/Dashboard/Settings/Settings';
-import Cart from './components/core/Dashboard/Cart/Cart';
-import { ACCOUNT_TYPE } from './utils/constants';
 import { useSelector } from 'react-redux';
-import { RootState } from './utils/store/store';
+import PrivateRoute from './components/core/Auth/PrivateRoute';
+import Dashboard from './components/pages/Dashboard';
+import Error from './components/pages/Error';
+import MyProfile from './components/core/Dashboard/MyProfile';
+import Settings from './components/core/Dashboard/Settings/Settings';
+import { ACCOUNT_TYPE } from './utils/constants';
+import Cart from './components/core/Dashboard/Cart/Cart';
+import EnrolledCourses from './components/core/Dashboard/EnrolledCourses';
 import AddCourse from './components/core/Dashboard/AddCourse/AddCourse';
 import MyCourses from './components/core/Dashboard/MyCourses/MyCourses';
 import EditCourse from './components/core/Dashboard/EditCourse/EditCourse';
-import MyProfile from './components/core/Dashboard/MyProfile';
-import Dashboard from './components/pages/Dashboard';
-import PrivateRoute from './components/core/Auth/PrivateRoute';
-import Error from './components/pages/Error';
-import EnrolledCourses from './components/core/Dashboard/EnrolledCourses';
-import PurchaseHistory from './components/core/Dashboard/PurchaseHistory';
-import Settings from './components/core/Dashboard/Settings/Settings';
-import Cart from './components/core/Dashboard/Cart/Cart';
-import { ACCOUNT_TYPE } from './utils/constants';
-import { useSelector } from 'react-redux';
 import { RootState } from './utils/store/store';
-import AddCourse from './components/core/Dashboard/AddCourse/AddCourse';
-import MyCourses from './components/core/Dashboard/MyCourses/MyCourses';
-import EditCourse from './components/core/Dashboard/EditCourse/EditCourse';
-import MyProfile from './components/core/Dashboard/MyProfile';
-import Dashboard from './components/pages/Dashboard';
-import PrivateRoute from './components/core/Auth/PrivateRoute';
-=======
->>>>>>> d73bdc5 (chore: added route for Error page if route is not defined)
 
 function App() {
   const { user } = useSelector((state: RootState) => state.profile);
@@ -109,8 +88,6 @@ function App() {
             </OpenRoute>
           }
         />
-<<<<<<< HEAD
-=======
 
         <Route
           element={
@@ -118,12 +95,32 @@ function App() {
               <Dashboard />
             </PrivateRoute>
           }
-        />
+        >
+          <Route path="dashboard/my-profile" element={<MyProfile />} />
+          <Route path="dashboard/settings" element={<Settings />} />
+
+          {user?.accountType === ACCOUNT_TYPE.STUDENT && (
+            <>
+              <Route path="dashboard/cart" element={<Cart />} />
+              <Route
+                path="dashboard/enrolled-courses"
+                element={<EnrolledCourses />}
+              />
+            </>
+          )}
+          {user?.accountType === ACCOUNT_TYPE.INSTRUCTOR && (
+            <>
+              <Route path="dashboard/add-course" element={<AddCourse />} />
+              <Route path="dashboard/my-courses" element={<MyCourses />} />
+              <Route
+                path="dashboard/edit-course/:courseId"
+                element={<EditCourse />}
+              />
+            </>
+          )}
+        </Route>
 
         <Route path="*" element={<Error />} />
-
->>>>>>> d73bdc5 (chore: added route for Error page if route is not defined)
-        <Route path="dashboard/my-profile" element={<MyProfile />} />
       </Routes>
     </div>
   );
