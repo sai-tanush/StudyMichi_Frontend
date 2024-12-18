6afc99e67c73efeb2daf9ca58a3c3b21dfcbcc81
S SAI TANUSH <saitanush789@gmail.com>
2024-12-18 16:32:35 +0530

    refactor: sanitation checks in TimelineSection component

--- src/components/core/Homepage/TimelineSection.tsx
+++ src/components/core/Homepage/TimelineSection.tsx
@@ -1,35 +1,5 @@
-import Logo1 from '../../../assets/TimeLineLogo/Logo1.svg';
-import Logo2 from '../../../assets/TimeLineLogo/Logo2.svg';
-import Logo3 from '../../../assets/TimeLineLogo/Logo3.svg';
-import Logo4 from '../../../assets/TimeLineLogo/Logo4.svg';
 import TimelineImage from '../../../assets/Images/TimelineImage.png';
-
-const timelineelements = [
-  {
-    Logo: Logo1,
-    Heading: 'Leadership',
-    Description: 'Fully committed to the success company',
-    line: true,
-  },
-  {
-    Logo: Logo2,
-    Heading: 'Responsibility',
-    Description: 'Students will always be our top priority',
-    line: true,
-  },
-  {
-    Logo: Logo3,
-    Heading: 'Flexibility',
-    Description: 'The ability to switch is an important skill',
-    line: true,
-  },
-  {
-    Logo: Logo4,
-    Heading: 'Solve the problem',
-    Description: 'Code your way to a solution',
-    line: false,
-  },
-];
+import { HOMEPAGE_TIMELINEELEMENTS_DATA } from '../../../data/homepage-data';
 
 const TimelineSection: React.FC = () => {
   return (
@@ -38,7 +8,7 @@ const TimelineSection: React.FC = () => {
       <div className="flex flex-row gap-5 items-center mt-5">
         {/* Left Section */}
         <div className="w-[60%] flex flex-col gap-5">
-          {timelineelements.map((element, index) => {
+          {HOMEPAGE_TIMELINEELEMENTS_DATA.map((element, index) => {
             return (
               <div className="flex flex-row gap-6" key={index}>
                 <div className="flex flex-col gap-2 items-center justify-center">
