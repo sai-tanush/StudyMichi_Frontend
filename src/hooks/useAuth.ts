db17d9f5264a0446f39383a5725cf40bd6a0af9b
S SAI TANUSH <saitanush789@gmail.com>
2024-12-18 13:11:59 +0530

    refactor: sanitation checks in useAuth

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,7 +1,7 @@
 import { useSelector } from 'react-redux';
 import { RootState } from '../utils/store/store'; // Adjust the path if needed
 
-const useCourse = () => {
+const useAuth = () => {
   const { token } = useSelector((state: RootState) => state.auth);
 
   // Determine if the user is authenticated based on token existence
@@ -10,4 +10,4 @@ const useCourse = () => {
   return { token, isAuthenticated };
 };
 
-export default useCourse;
+export default useAuth;
