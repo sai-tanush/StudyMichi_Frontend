2e80a25cd77c7994fd71e94f2cbdcf7fc4675a3f
S SAI TANUSH <saitanush789@gmail.com>
2024-12-18 12:14:02 +0530

    refactor: sanitation checks in PrivateRoute componnet

--- src/components/core/Auth/PrivateRoute.tsx
+++ src/components/core/Auth/PrivateRoute.tsx
@@ -1,6 +1,8 @@
+// This component will ensure only authenticated users can access this route
+
 import { useSelector } from 'react-redux';
-import { RootState } from '../../../utils/store/store';
 import { Navigate } from 'react-router-dom';
+import { RootState } from '../../../utils/store/store';
 
 interface PrivateRouteProps {
   children: React.ReactNode;
