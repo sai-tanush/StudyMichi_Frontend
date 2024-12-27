f40e2f6f92aeec28e204b5ead7163e15edb2b714
S SAI TANUSH <saitanush789@gmail.com>
2024-12-27 20:12:58 +0530

    chore: defined buyCourse function in BuyCourseCard component

--- src/components/core/Catalog/BuyCourseCard.tsx
+++ src/components/core/Catalog/BuyCourseCard.tsx
@@ -1,8 +1,25 @@
 import { FaShareFromSquare } from 'react-icons/fa6';
 import Thumbnail from '../../../assets/Images/aboutus1.webp';
+import { buyCourse } from '../../../services/operations/studentFeaturesAPI';
+import useAuth from '../../../hooks/useAuth';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import useUserDetails from '../../../hooks/useUserDetails';
 
 const BuyCourseCard: React.FC = () => {
-  const handleBuyCourse = () => {};
+  const { user } = useUserDetails();
+  const { token } = useAuth();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { courseId } = useParams();
+
+  const handleBuyCourse = () => {
+    console.log('token in handleBuyCourse = ', token);
+    if (token) {
+      buyCourse([courseId], token, user, navigate, dispatch);
+      return;
+    }
+  };
 
   const handleAddtoCart = () => {};
 
@@ -16,13 +33,13 @@ const BuyCourseCard: React.FC = () => {
       <p className="text-4xl text-richblack-5 font-semibold">Rs. 699</p>
 
       <button
-        onClick={() => handleBuyCourse}
+        onClick={handleBuyCourse}
         className="text-center text-[16px] px-6 py-3 rounded-md font-bold bg-yellow-50 text-black hover:scale-95 transition-all duration-200 shadow-sm shadow-richblack-300"
       >
         Buy now
       </button>
       <button
-        onClick={() => handleAddtoCart}
+        onClick={handleAddtoCart}
         className="text-center text-[16px] px-6 py-3 rounded-md font-bold bg-richblack-800 text-white hover:scale-95 
         transition-all duration-200 shadow-sm shadow-richblack-600 border border-richblack-600"
       >
