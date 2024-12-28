8d5fc95c6b61e97bb59108bd9f6db90cdadc8b62
S SAI TANUSH <saitanush789@gmail.com>
2024-12-28 20:39:48 +0530

    chore: added conditional rendering in SubSectionCard component

--- src/components/core/CourseDetails/SubSectionCard.tsx
+++ src/components/core/CourseDetails/SubSectionCard.tsx
@@ -5,17 +5,27 @@ import { FaChevronDown, FaChevronUp } from 'react-icons/fa';
 
 interface SubSectionCardProps {
   subsection: SubSectionProps;
+  collapseSections: boolean;
+  setCollapseSections: React.Dispatch<React.SetStateAction<boolean>>;
 }
-const SubSectionCard: React.FC<SubSectionCardProps> = ({ subsection }) => {
-  const [showDescription, setShowDescription] = useState<boolean>(false);
+const SubSectionCard: React.FC<SubSectionCardProps> = ({
+  subsection,
+  collapseSections,
+  setCollapseSections,
+}) => {
   const [timeDuration, setTimeDuration] = useState<number>(0);
-
+  const [showDescription, setShowDescription] = useState<boolean>(false);
   const lectureTimeDuration = subsection?.timeDuration;
 
   const convertIntoMin = (lectureTimeDuration: string) => {
     let subSectionTimeDuration = 0;
     subSectionTimeDuration = Math.floor(parseInt(lectureTimeDuration) / 60);
     setTimeDuration(subSectionTimeDuration);
+  };
+
+  const handleShowDescription = () => {
+    setCollapseSections(false);
+    setShowDescription((val) => !val);
   };
 
   useEffect(() => {
@@ -25,27 +35,36 @@ const SubSectionCard: React.FC<SubSectionCardProps> = ({ subsection }) => {
   console.log('subSection in SubSectionCard = ', subsection);
 
   return (
-    <div className="bg-richblack-800 lg:h-16 flex justify-between mb-2">
-      <div className="flex justify-between items-center">
-        <div
-          className="flex items-center gap-x-3 ml-10"
-          onClick={() => setShowDescription((val) => !val)}
-        >
-          <div className="text-richblack-300">
-            <MdVideoLibrary size={34} />
+    <>
+      {!collapseSections && (
+        <div className="bg-richblack-800 lg:min-h-16 flex flex-col justify-between mb-2">
+          <div className="w-full flex justify-between items-center pt-4">
+            <div
+              className="flex justify-center items-center gap-x-3 ml-10"
+              onClick={handleShowDescription}
+            >
+              <div className="text-richblack-300">
+                <MdVideoLibrary size={34} />
+              </div>
+              <p className="text-richblack-5">{subsection?.title}</p>
+              <div className="text-richblack-100">
+                {!showDescription && !collapseSections ? (
+                  <FaChevronDown size={12} />
+                ) : (
+                  <FaChevronUp size={12} />
+                )}
+              </div>
+            </div>
+            <div className="mr-10 text-richblack-100">{timeDuration} mins</div>
           </div>
-          <p className="text-richblack-100">{subsection?.title}</p>
-          <div className="text-richblack-100">
-            {!showDescription ? (
-              <FaChevronDown size={12} />
-            ) : (
-              <FaChevronUp size={12} />
-            )}
-          </div>
+          {showDescription && (
+            <p className="text-richblack-100 ml-20 my-4">
+              {subsection?.description}
+            </p>
+          )}
         </div>
-        <div className="mr-10 text-richblack-100">{timeDuration} mins</div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
