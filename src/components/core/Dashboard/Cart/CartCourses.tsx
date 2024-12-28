9281700dce8432bfec8d69879bb071092755d3e5
S SAI TANUSH <saitanush789@gmail.com>
2024-12-28 13:16:21 +0530

    style: added styling in CartCourses component

--- src/components/core/Dashboard/Cart/CartCourses.tsx
+++ src/components/core/Dashboard/Cart/CartCourses.tsx
@@ -9,49 +9,56 @@ import { RootState } from '../../../../utils/store/store';
 const CartCourses = () => {
   const { cart } = useSelector((state: RootState) => state.cart);
   const dispatch = useDispatch();
+  console.log('cart Details = ', cart);
 
   return (
-    <div>
+    <div className="w-[40%]">
       {cart.map((course, index) => (
-        <div>
+        <div className="flex justify-between items-center border border-richblack-600 rounded-lg p-2 mb-6">
           <div key={index} className="flex items-center gap-4 p-4 border-b">
             {/* Course Thumbnail */}
             <img
               src={course?.thumbnail}
               alt={`${course?.courseName} thumbnail`}
-              className="w-20 h-20 object-cover rounded-md"
+              className="w-[250px] h-[150px] object-cover rounded-md"
             />
 
             {/* Course Details */}
             <div className="flex flex-col">
               {/* Course Name */}
-              <p className="text-lg font-semibold">{course?.courseName}</p>
+              <p className="text-lg font-semibold text-richblack-5">
+                {course?.courseName}
+              </p>
               {/* Course Category */}
-              <p className="text-sm text-gray-500">{course?.category?.name}</p>
+              <p className="text-sm text-gray-500 text-richblack-5">
+                {course?.category?.name}
+              </p>
               {/* Rating */}
               <div className="flex items-center mt-2">
-                <span className="text-yellow-500 text-sm font-bold mr-2">
+                <span className="text-yellow-200 text-sm font-bold mr-2">
                   4.8
                 </span>
                 <ReactStars
                   count={5} // Total stars
                   size={20} // Size of each star
+                  value={4}
                   edit={false} // Set to false to make the stars uneditable
                   activeColor="#ffd700" // Active star color (filled star)
                   emptyIcon={<GiNinjaStar />} // Icon for empty star
                   fullIcon={<GiNinjaStar />} // Icon for filled star
                 />
-                <span>{course?.ratingAndReviews?.length} Ratings</span>
+                <span className="text-richblack-300 text-sm">
+                  {course?.ratingAndReviews?.length} Ratings
+                </span>
               </div>
+              <p className="text-caribbeangreen-50">Rs {course?.price} </p>
             </div>
           </div>
 
-          <div>
+          <div className="text-red-100">
             <button onClick={() => dispatch(removeFromCart(course._id))}>
-              <RiDeleteBin6Line />
-              <span>Remove</span>
+              <RiDeleteBin6Line size={22} />
             </button>
-            <p>Rs {course?.price} </p>
           </div>
         </div>
       ))}
