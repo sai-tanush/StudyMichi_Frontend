dff5e5d803a39b6e845e0b7e4e4df49897aae2f2
S SAI TANUSH <saitanush789@gmail.com>
2024-12-28 13:15:24 +0530

    style: added additional styling in Cart component

--- src/components/core/Dashboard/Cart/Cart.tsx
+++ src/components/core/Dashboard/Cart/Cart.tsx
@@ -6,7 +6,7 @@ import CartAmount from './CartTotalAmount';
 const Cart = () => {
   const { total, totalItems } = useSelector((state: RootState) => state.cart);
   return (
-    <div className="lg:w-[60%] ml-[10%]">
+    <div className="lg:w-[70%] ml-[10%]">
       <h1 className="mb-14 text-3xl font-semibold text-richblack-5">
         Your Cart
       </h1>
@@ -14,7 +14,7 @@ const Cart = () => {
         Total {totalItems} courses in cart
       </p>
       {total > 0 ? (
-        <div className="mt-8 flex flex-col-reverse items-start gap-x-10 gap-y-6 lg:flex-row">
+        <div className="mt-8 flex flex-col-reverse items-start justify-around gap-x-10 gap-y-6 lg:flex-row">
           <CartCourses />
           <CartAmount />
         </div>
