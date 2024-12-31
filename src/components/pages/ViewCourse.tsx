765822bb9b331d988c669a83a698caf23f48871a
S SAI TANUSH <saitanush789@gmail.com>
2024-12-31 13:18:22 +0530

    chore: updated functionality in VideoCourse component

--- src/components/pages/ViewCourse.tsx
+++ src/components/pages/ViewCourse.tsx
@@ -1,5 +1,50 @@
+import { useEffect, useState } from 'react';
+import VideoDetailsSidebar from '../core/ViewCourse/VideoDetailsSidebar';
+import { Outlet, useParams } from 'react-router-dom';
+import CourseReviewModal from '../core/ViewCourse/CourseReviewModal';
+import useAuth from '../../hooks/useAuth';
+import { useDispatch } from 'react-redux';
+import { getFullDetailsOfCourse } from '../../services/operations/courseDetailsAPI';
+import {
+  setCompletedLectures,
+  setCourseSectionData,
+  setEntireCourseData,
+  setTotalNoOfLectures,
+} from '../../utils/slices/viewCourseSlice';
+
 const ViewCourse: React.FC = () => {
-  return <div></div>;
+  const [reviewModal, setReviewModal] = useState(false);
+  const { courseId } = useParams();
+  const { token } = useAuth();
+  const dispatch = useDispatch();
+
+  const setCourseSpecificDetails = async () => {
+    const courseData = await getFullDetailsOfCourse(courseId, token);
+    dispatch(setCourseSectionData(courseData?.courseDetails?.courseContent));
+    dispatch(setEntireCourseData(courseData?.courseDetails));
+    dispatch(setCompletedLectures(courseData?.completedVideos));
+    let lectures = 0;
+    courseData?.courseDetails?.courseContent?.forEach((sec) => {
+      lectures += sec.subSection.length;
+    });
+    dispatch(setTotalNoOfLectures(lectures));
+  };
+
+  useEffect(() => {
+    setCourseSpecificDetails();
+  }, []);
+
+  return (
+    <div>
+      <div>
+        <VideoDetailsSidebar setReviewModal={setReviewModal} />
+        <div>
+          <Outlet />
+        </div>
+      </div>
+      {reviewModal && <CourseReviewModal setReviewModal={setReviewModal} />}
+    </div>
+  );
 };
 
 export default ViewCourse;
