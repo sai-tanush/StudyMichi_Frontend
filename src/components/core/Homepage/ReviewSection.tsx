948ab069262d809ae3b282f43e716c6aef863166
S SAI TANUSH <saitanush789@gmail.com>
2025-01-03 18:31:15 +0530

    style: added styling for ReviewSection component

--- src/components/core/Homepage/ReviewSection.tsx
+++ src/components/core/Homepage/ReviewSection.tsx
@@ -3,7 +3,7 @@ import ReviewSlider from '../../common/ReviewSlider';
 const ReviewSection = () => {
   return (
     <div>
-      <div className="text-4xl font-bold text-center mt-[150px] ">
+      <div className="text-xl lg:text-4xl font-bold text-center mt-10 lg:mt-[150px] ">
         Reviews from other learners
       </div>
       {/* Review Cards */}
