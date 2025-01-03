df5d4957da63421b702d1964e584711fd322a320
S SAI TANUSH <saitanush789@gmail.com>
2025-01-03 18:30:57 +0530

    style: added styling for TimelineSection component

--- src/components/core/Homepage/TimelineSection.tsx
+++ src/components/core/Homepage/TimelineSection.tsx
@@ -7,7 +7,7 @@ const TimelineSection: React.FC = () => {
       {/* Outer Big Box */}
       <div className="flex flex-row gap-5 items-center mt-5">
         {/* Left Section */}
-        <div className="w-[60%] flex flex-col gap-5">
+        <div className="lg:w-[60%] ml-6 lg:ml-0 flex flex-col gap-5">
           {HOMEPAGE_TIMELINEELEMENTS_DATA.map((element, index) => {
             return (
               <div className="flex flex-row gap-6" key={index}>
@@ -38,24 +38,30 @@ const TimelineSection: React.FC = () => {
           <img
             src={TimelineImage}
             alt="Timeline Image"
-            className="shadow-2xl shadow-blue-200"
+            className="shadow-2xl shadow-blue-200 hidden lg:block"
           />
           <div
-            className="absolute bg-caribbeangreen-700 flex flex-row text-white uppercase py-7 px-10 
-          left-[50%] translate-x-[-50%] translate-y-[-50%]"
+            className="absolute bg-caribbeangreen-700 flex lg:flex-row text-white uppercase py-4 lg:py-7 px-5 lg:px-10 
+            lg:left-[50%] translate-x-[-110%] lg:translate-x-[-50%] translate-y-[340%] lg:translate-y-[-50%]"
           >
             <div className="flex flex-row gap-4">
-              <p className="text-3xl font-bold">10</p>
+              <p className="text-lg lg:text-3xl font-bold">10</p>
               <div className="flex flex-col gap-2 border-r border-caribbeangreen-500">
-                <p className="text-caribbeangreen-300 text-sm pr-5">Years</p>
-                <p className="text-caribbeangreen-300 text-sm pr-5">
+                <p className="text-caribbeangreen-300 text-xs lg:text-sm pr-5">
+                  Years
+                </p>
+                <p className="text-caribbeangreen-300 text-xs lg:text-sm pr-5">
                   Experience
                 </p>
               </div>
-              <p className="text-3xl font-bold">250</p>
+              <p className="text-lg lg:text-3xl font-bold">250</p>
               <div className="flex flex-col gap-2">
-                <p className="text-caribbeangreen-300 text-sm">Types of</p>
-                <p className="text-caribbeangreen-300 text-sm">Courses</p>
+                <p className="text-caribbeangreen-300 text-xs lg:text-sm">
+                  Types of
+                </p>
+                <p className="text-caribbeangreen-300 text-xs lg:text-sm">
+                  Courses
+                </p>
               </div>
             </div>
           </div>
