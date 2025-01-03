186ca43ea32700d67fe05658f96145a196a772d9
S SAI TANUSH <saitanush789@gmail.com>
2025-01-03 17:48:45 +0530

    style: added styling for ContactFormSection component

--- src/components/core/AboutUs/ContactFormSection.tsx
+++ src/components/core/AboutUs/ContactFormSection.tsx
@@ -10,12 +10,16 @@ const ContactFormSection: React.FC<ContactFormSectionProps> = ({
   paragraph,
 }) => {
   return (
-    <div className="flex flex-col gap-y-4 text-center mx-auto mb-28 border border-richblack-400 p-10 rounded-xl">
+    <div className="w-[93%] lg:w-fit flex flex-col gap-y-4 -mt-36 lg:-mt-0 text-center mx-auto mb-28 border border-richblack-400 px-6 py-6 lg:p-10 rounded-xl">
       <div className="flex flex-col gap-y-2 mb-10">
-        <h1 className="text-4xl font-bold text-richblack-5">{title}</h1>
-        <p className="text-richblack-400 text-md font-semibold">{paragraph}</p>
+        <h1 className="text-xl lg:text-4xl font-bold text-richblack-5">
+          {title}
+        </h1>
+        <p className="text-richblack-400 text-sm lg:text-md font-semibold">
+          {paragraph}
+        </p>
       </div>
-      <div>
+      <div className="w-[60%] lg:w-auto">
         <ContactUsForm />
       </div>
     </div>
