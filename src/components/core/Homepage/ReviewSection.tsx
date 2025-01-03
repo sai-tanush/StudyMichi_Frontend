45d2a40683360198694a2f3d9a5e0cc9c7cbb6a9
S SAI TANUSH <saitanush789@gmail.com>
2025-01-03 09:14:00 +0530

    chore: added ReviewSlider component in ReviewSection for Homepage component

--- src/components/core/Homepage/ReviewSection.tsx
+++ src/components/core/Homepage/ReviewSection.tsx
@@ -1,11 +1,13 @@
+import ReviewSlider from '../../common/ReviewSlider';
+
 const ReviewSection = () => {
   return (
     <div>
-      <div className="text-4xl font-bold text-center mt-[150px]">
+      <div className="text-4xl font-bold text-center mt-[150px] ">
         Reviews from other learners
       </div>
       {/* Review Cards */}
-      <div></div>
+      <ReviewSlider />
     </div>
   );
 };
