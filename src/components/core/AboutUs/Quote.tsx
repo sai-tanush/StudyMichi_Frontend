7fb23bef743578f44f6e6bea0f82a3306895a7a3
S SAI TANUSH <saitanush789@gmail.com>
2025-01-03 17:49:26 +0530

    style: added styling for Quote component

--- src/components/core/AboutUs/Quote.tsx
+++ src/components/core/AboutUs/Quote.tsx
@@ -2,17 +2,17 @@ import HighlightText from '../Homepage/HighlightText';
 
 const Quote: React.FC = () => {
   return (
-    <div className="text-center text-4xl font-bold">
+    <div className="text-center lg:text-4xl text-lg font-bold">
       <h1 className="text-richblack-100 leading-normal ">
-        <span className="text-richblack-500 text-5xl">" </span>
+        <span className="text-richblack-500 text-lg lg:text-5xl">" </span>
         We are passionate about revolutionizing the way we learn. Our <br></br>
         innovative platform <HighlightText text={` combines texhnology`} />,
-        <span className="font-bold text-transparent bg-clip-text bg-gradient-to-b from-[#FF512F] to-[#F09819]">
+        <span className="text-lg lg:text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-b from-[#FF512F] to-[#F09819]">
           {' '}
           expertise
         </span>{' '}
         and community to <br></br>create an{' '}
-        <span className="font-bold text-transparent bg-clip-text bg-gradient-to-b from-[#E65C00] to-[#F9D423]">
+        <span className="text-lg lg:text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-b from-[#E65C00] to-[#F9D423]">
           unparalleled educational experience.
         </span>
         <span className="text-richblack-500"> "</span>
