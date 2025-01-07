d9aefb70144cd2fd2fcf5837ab872f8bfdab7efc
S SAI TANUSH <saitanush789@gmail.com>
2025-01-07 12:31:06 +0530

    refactor: sanitation checks in avgRating function, added props for ratingArr prop

--- src/utils/helperFunctions/avgRating.ts
+++ src/utils/helperFunctions/avgRating.ts
@@ -1,14 +1,23 @@
-export default function GetAvgRating(ratingArr) {
-  if (ratingArr?.length === 0) return 0;
-  const totalReviewCount = ratingArr?.reduce((acc, curr) => {
-    acc += curr.rating;
-    return acc;
-  }, 0);
+import { ReviewAndRatingProps } from '../slices/courseSlice';
+
+interface RatingArrProps {
+  ratingArr: ReviewAndRatingProps[];
+}
+
+export default function GetAvgRating({ ratingArr }: RatingArrProps): number {
+  if (ratingArr.length === 0) return 0;
+
+  const totalReviewCount = ratingArr.reduce(
+    (acc: number, curr: ReviewAndRatingProps) => {
+      acc += curr.rating;
+      return acc;
+    },
+    0,
+  );
 
   const multiplier = Math.pow(10, 1);
   const avgReviewCount =
-    Math.round((totalReviewCount / ratingArr?.length) * multiplier) /
-    multiplier;
+    Math.round((totalReviewCount / ratingArr.length) * multiplier) / multiplier;
 
   return avgReviewCount;
 }
