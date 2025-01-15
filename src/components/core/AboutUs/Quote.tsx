cde12d65d546cb6341de1b29f785d5ecfd67e903
S SAI TANUSH <saitanush789@gmail.com>
2025-01-15 13:43:16 +0530

    refcator: sanitation changes in Quote Component

--- src/components/core/AboutUs/Quote.tsx
+++ src/components/core/AboutUs/Quote.tsx
@@ -6,7 +6,7 @@ const Quote: React.FC = () => {
       <h1 className="text-richblack-100 leading-normal ">
         <span className="text-richblack-500 text-lg lg:text-5xl">" </span>
         We are passionate about revolutionizing the way we learn. Our <br></br>
-        innovative platform <HighlightText text={` combines texhnology`} />,
+        innovative platform <HighlightText text={` combines technology`} />,
         <span className="text-lg lg:text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-b from-[#FF512F] to-[#F09819]">
           {' '}
           expertise
